ae5e750d5f36632aae6b9f90a88bd31b52758ca3
mansari25 <ansari.mack>
2017-06-05 09:57:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mackansari/maqbool2017summer/NewActivity.java
@@ -0,0 +1,17 @@
+package com.example.mackansari.maqbool2017summer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mackansari on 6/5/17.
+ */
+
+public class NewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
