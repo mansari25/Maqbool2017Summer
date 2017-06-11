bc2d0dadbfe513908f8cee2e373c961e45ad9887
mackansari <ansari.mack@gmail.com>
2017-06-11 19:19:38 -0400

    6/11  2nd commit

--- app/src/main/java/com/example/mackansari/maqbool2017summer/fragment/DemoFragment.java
+++ app/src/main/java/com/example/mackansari/maqbool2017summer/fragment/DemoFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.example.mackansari.maqbool2017summer.R;
+import com.example.mackansari.maqbool2017summer.adapter.ListNormalAdapter;
 
 import java.util.ArrayList;
 
@@ -23,16 +24,16 @@ public class DemoFragment extends Fragment {
         contentList = new ArrayList<String>();
         contentList.add("ViewPager");
         contentList.add("imageScaleType");
-        contentList.add("(9Patch");
-        contentList.add("(9Patch");
-        contentList.add("(9Patch");
-        contentList.add("(9Patch");
-        contentList.add("(9Patch");
-        contentList.add("(9Patch");
-        contentList.add("(9Patch");
-        contentList.add("(9Patch");
-        contentList.add("(9Patch");
-        contentList.add("(9Patch");
+        contentList.add("9Patch");
+        contentList.add("A");
+        contentList.add("B");
+        contentList.add("C");
+        contentList.add("D");
+        contentList.add("E");
+        contentList.add("F");
+        contentList.add("G");
+        contentList.add("H");
+        contentList.add("I");
 
     }
 
@@ -50,7 +51,9 @@ public class DemoFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_demo,container,false);
         listView = (ListView)view.findViewById(R.id.fragment_demo_lv);
-        return inflater.inflate(R.layout.fragment_demo, container, false);
+        ListNormalAdapter adapter = new ListNormalAdapter(this.getContext(), contentList);
+        listView.setAdapter(adapter);
+        return view;
     }
 
 }
