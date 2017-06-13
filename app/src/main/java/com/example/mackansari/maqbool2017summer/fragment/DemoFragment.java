99f8fab362a3c35be6138fd23fd7fbbdab09fc09
mackansari <ansari.mack@gmail.com>
2017-06-13 17:20:55 -0400

    6/13 commit

--- app/src/main/java/com/example/mackansari/maqbool2017summer/fragment/DemoFragment.java
+++ app/src/main/java/com/example/mackansari/maqbool2017summer/fragment/DemoFragment.java
@@ -1,26 +1,34 @@
 package com.example.mackansari.maqbool2017summer.fragment;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.example.mackansari.maqbool2017summer.LaunchModeActivity;
 import com.example.mackansari.maqbool2017summer.R;
+import com.example.mackansari.maqbool2017summer.ScaleTypeActivity;
 import com.example.mackansari.maqbool2017summer.adapter.ListNormalAdapter;
-
+import com.example.mackansari.maqbool2017summer.ViewPagerActivity;
 import java.util.ArrayList;
 
 
 public class DemoFragment extends Fragment {
 
     private final ArrayList<String> contentList;
+    private final Context context;
     private ListView listView;
 
 
     public DemoFragment() {
+        context = getContext();
         contentList = new ArrayList<String>();
         contentList.add("ViewPager");
         contentList.add("imageScaleType");
@@ -28,7 +36,7 @@ public class DemoFragment extends Fragment {
         contentList.add("A");
         contentList.add("B");
         contentList.add("C");
-        contentList.add("D");
+        contentList.add("LaunchMode");
         contentList.add("E");
         contentList.add("F");
         contentList.add("G");
@@ -53,6 +61,27 @@ public class DemoFragment extends Fragment {
         listView = (ListView)view.findViewById(R.id.fragment_demo_lv);
         ListNormalAdapter adapter = new ListNormalAdapter(this.getContext(), contentList);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        Intent intent = new Intent(getActivity(), ViewPagerActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        Intent intent1 = new Intent(getActivity(), ScaleTypeActivity.class);
+                        startActivity(intent1);
+                        break;
+                    case 6:
+                        Intent intent6 = new Intent(getActivity(), LaunchModeActivity.class);
+                        startActivity(intent6);
+                    default:
+
+                }
+            }
+        });
         return view;
     }
 
